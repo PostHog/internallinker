5cc685eac44bbefc14af523de7a259dc495b26ac
James Hawkins <james@posthog.com>
2021-12-25 03:26:16 +0000

    got filepaths

--- links.py
+++ links.py
@@ -20,21 +20,16 @@ blogText = ''
 
 for filename in glob.glob(os.path.join(path, '*.md')):
     with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
+        filepath = os.path.splitext(f.name)[0]
         frontMatter, blogText = list(yaml.load_all(f, Loader=yaml.FullLoader))[:2]
         keywords = frontMatter["keywords"].split(",")
-        print(keywords)
-# Get the file path
-filepath = os.path.splitext("example-post.md")[0]
-
-# Save these keywords into a list
-for keyword in keywords:
-    keywordAssociations[filepath] = [keyword]
+        for keyword in keywords:
+            keywordAssociations[filepath] = [keyword]
 
 # Insert link whenever a keyword is found
-
 for key, value in keywordAssociations.items():
     for i in value:
         newText = "["+i+"]("+key+")"
         blogText = blogText.replace(i,newText)
 
-print(blogText)
+# Todo, the output of the above should be to actually replace the keywords in the articles with links. ie don't open the file in read only mode and write to it
