2827ccd3e2d12cb0e8c67c0e98c448a2650e99c0
James Hawkins <james@posthog.com>
2021-12-28 14:57:26 +0000

    replace blog text with new blog text and save

--- generate-links.py
+++ generate-links.py
@@ -2,7 +2,7 @@ import yaml
 import json
 import os
 import glob
-
+import re
 # CONFIGURATION
 # Set path to the folder containing the blog posts
 #todo: make this re-use the config from generate-keywords.py
@@ -11,16 +11,16 @@ path = 'blog'
 
 jsonFile = open('keywordAssociations.json')
 keywordAssociations = json.load(jsonFile)
-print(keywordAssociations)
 
 for file in glob.glob(os.path.join(path, '*.md')):
-    with open(os.path.join(os.getcwd(), file), 'r') as f:
+    with open(os.path.join(os.getcwd(), file), 'r+') as f:
         frontMatter, blogText = list(yaml.load_all(f, Loader=yaml.FullLoader))[:2]
-
         for key, value in keywordAssociations.items():
             for url, linkedKeyword in value.items():
-                print(url)
-                print(linkedKeyword)
                 newText = "["+linkedKeyword+"]("+url+")"
                 blogText = blogText.replace(linkedKeyword,newText)
-                print(blogText)
+                markdownContent = "---\n"+yaml.dump(frontMatter) + "---\n" + blogText
+        # replace the file contents
+        f.truncate(0)
+        f.seek(0) #avoids weird characters at the start
+        f.write(markdownContent)
